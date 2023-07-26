3b7bdb805c237490c97e69486ad9010aa8d6cf5d
NathanW1010 <nmweiland@winsupplyinc.com>
2023-07-26 14:10:24 -0400

    Updated education list

--- src/app/education-list.ts
+++ src/app/education-list.ts
@@ -2,7 +2,25 @@ import {Education} from './interface/education';
 
 export const EDUCATION: Education[] = [
   {
-    name: 'Wright State University',
+    name: 'Wright State University (Graduate)',
+    startDate: 'Spring 2023',
+    endDate: 'Present',
+    activities: [
+      'Teaching Assistant / Lab Leader',
+      'Student Ambassador',
+      'Student Conduct Panel Member',
+    ],
+    photoCreditName: 'Bob Mihalek',
+    iconSm: 'wsu-sm-compressed.jpg',
+    iconMd: 'wsu-md-compressed.jpg',
+    iconLg: 'wsu-lg-compressed.jpg',
+    iconXl: 'wsu-xl-compressed.jpg',
+    GPA: '4.000',
+    degree: 'Master of Science in Computer Science (MSCS)',
+    location: '3640 Colonel Glenn Hwy, Dayton, OH 45435'
+  },
+  {
+    name: 'Wright State University (Undergraduate)',
     startDate: 'August 2020',
     endDate: 'Present',
     activities: [
@@ -11,7 +29,8 @@ export const EDUCATION: Education[] = [
       'Student Conduct Panel Member',
       'Campus Scholarship and Innovation Campaign (CSIC)',
       'Annual Fall Mailing Photoshoot',
-      'Spring 2023 Appeal Photoshoot'
+      'Spring 2023 Appeal Photoshoot',
+      '4+1 Program (Taking graduate level courses)'
     ],
     photoCreditName: 'Bob Mihalek',
     iconSm: 'wsu-sm-compressed.jpg',
